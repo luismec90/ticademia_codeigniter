631403b9af44a229245134b4ee5650716f40a5c5
Luis Fernando Montoya <luismec90@gmail.com>
2014-11-07 15:11:15 -0500

    Mejoras

--- resources/1/19/129/js/main.js
+++ resources/1/19/129/js/main.js
@@ -1,52 +1,63 @@
-var m,k;
+var m, k;
 
-$(function() {
-	try{
-		API = getAPI();
-		API.LMSInitialize("");
-	}catch(e){
-		console.log(e);
-	}
+$(function () {
+    try{
+     API = getAPI();
+     API.LMSInitialize("");
+     }catch(e){
+     console.log(e);
+     }
 
-    m = getRandom(2,16);
-    k = getRandom(1,15);
+    m = getRandom(2, 16);
+    k = getRandom(1, 15);
 
-    var correctAnswer1 = "(";
-    var correctAnswer2 = -m*k;
-    var correctAnswer3 = m*k-1;
-    var correctAnswer4 = -1;
-    var correctAnswer5 = ")";
+   console.log((m+1) + " " + (k+1));
+
+    var correctAnswer1 = "(-inf";
+    var correctAnswer2 = "-1";
+    var correctAnswer3 = ")";
+    var correctAnswer4 = "(";
+    var correctAnswer5 = -m * k;
+    var correctAnswer6 = m * k + 1;
+    var correctAnswer7 = "inf)"
+
+
     //var missConception1 = n;
     //console.log(correctAnswer1);
     //console.log(correctAnswer1 + " " + correctAnswer2);
     draw();
 
-    $("#verificar").click(function() {
+    $("#verificar").click(function () {
         var valor1 = $("#answer1").val().trim();
         var valor2 = $("#answer2").val().trim();
         var valor3 = $("#answer3").val().trim();
         var valor4 = $("#answer4").val().trim();
         var valor5 = $("#answer5").val().trim();
-        if (valor1 != "" && valor2 != "" && valor3 != "" && valor4 != "" && valor5 != "") {
+        var valor6 = $("#answer6").val().trim();
+        var valor7 = $("#answer7").val().trim();
+
+        if (valor1 != "" && valor2 != "" && valor3 != "" && valor4 != "" && valor5 != "" && valor6 != "" && valor7 != "") {
             $("#correcto").addClass("hide");
             $("#feedback").addClass("hide");
             var calificacion = 0;
             var feedback = "";
             valor2 = parseFloat(valor2);
-            valor3 = parseFloat(valor3);
-            valor4 = parseFloat(valor4);
-			
-			if(valor1 == correctAnswer1 &&
+            valor5 = parseFloat(valor5);
+            valor6 = parseFloat(valor6);
+
+            if (valor1 == correctAnswer1 &&
                 valor2 == correctAnswer2 &&
                 valor3 == correctAnswer3 &&
                 valor4 == correctAnswer4 &&
-                valor5 == correctAnswer5){
-				calificacion = 1.0;
-				$("#correcto").html("Calificación: <b>" + calificacion + "</b>").removeClass("hide");
-            }else{
-				calificacion = 0.0;
-				$("#feedback").html("Calificación: <b>" + calificacion + "</b> <br> ...").removeClass("hide");
+                decimalComparison(valor5/valor6,correctAnswer5/correctAnswer6,2) &&
+                valor7 == correctAnswer7) {
+                calificacion = 1.0;
+                $("#correcto").html("Calificación: <b>" + calificacion + "</b>").removeClass("hide");
+            } else {
+                calificacion = 0.0;
+                $("#feedback").html("Calificación: <b>" + calificacion + "</b> <br> ...").removeClass("hide");
             }
+
             $(this).attr("disabled", true);
             API.closeQuestion();
             if (typeof API.calificar == 'function') {
@@ -57,10 +68,10 @@ $(function() {
             API.notifyDaemon(calificacion);
         }
     });
-    $("#aceptar").click(function() {
+    $("#aceptar").click(function () {
         window.parent.location.reload();
     });
-    $('#modal').on('hide.bs.modal', function(e) {
+    $('#modal').on('hide.bs.modal', function (e) {
         window.parent.location.reload();
     });
 
@@ -68,8 +79,8 @@ $(function() {
 function getRandom(bottom, top) {
     return Math.floor(Math.random() * (1 + top - bottom)) + bottom;
 }
-function getRandomFrom(vals){
-	return vals[getRandom(0,vals.length-1)];
+function getRandomFrom(vals) {
+    return vals[getRandom(0, vals.length - 1)];
 }
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -80,13 +91,13 @@ function shuffleArray(array) {
     }
     return array;
 }
-function decimalComparison(v1,v2,d){
-    d = Math.pow(10,d);
-    v1 = Math.round(v1*d);
-    v2 = Math.round(v2*d);
-    return v1==v2;
+function decimalComparison(v1, v2, d) {
+    d = Math.pow(10, d);
+    v1 = Math.round(v1 * d);
+    v2 = Math.round(v2 * d);
+    return v1 == v2;
 }
-function draw(){
+function draw() {
     $('.mvar[value=e1]').html(m);
     $('.mvar[value=e2]').html(k);
 }
